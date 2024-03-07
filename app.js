e5014c0ca22e63db452f672187f683b03db0656d
Patricknshimiyimana <lennoxmupi@gmail.com>
2024-03-07 16:34:51 +0200

    initialize express app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    }
+);
+
+app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+    }
+);
