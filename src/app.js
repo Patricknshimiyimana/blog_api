d205b7659893156d00bc10dceecd5fe74cfdb2e6
Patricknshimiyimana <lennoxmupi@gmail.com>
2024-03-09 15:52:54 +0200

    setup sequelize and postgres db & create blog endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import { Sequelize } from 'sequelize';
 import appRouter from './routes';
 
 dotenv.config();
@@ -7,6 +9,34 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Connect to PostgreSQL using Sequelize
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "postgres",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+  );
+  
+  // Test the database connection
+  sequelize
+    .authenticate()
+    .then(() => {
+      console.log("Connected to PostgreSQL database");
+    })
+    .catch((error) => {
+      console.error("Unable to connect to the database:", error);
+    });
+
+app.use(bodyParser.json());
 app.use(appRouter)
 
 app.listen(port, () => {
